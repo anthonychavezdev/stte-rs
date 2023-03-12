d325365dd410d8add8453664def15693a9d0ad53
Anthony Chavez <anthony@anthonychavez.xyz>
2023-03-11 21:34:08 -0600

    Clear the screen on exit or error

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ struct CleanUp;
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Could not turn off raw mode");
+        Screen::clear().expect("Error");
     }
 }
 
